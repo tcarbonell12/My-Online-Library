d4a05f28947334bda21ef0e3fdaeaf1f84f673bf
tcarbonell12 <thomas.carbonell@live.lagcc.cuny.edu>
2017-12-05 18:27:24 -0500

    Add files via upload

---
+++ myOnlineLibrary/inc/data.php
@@ -0,0 +1,152 @@
+<?php
+//Books
+
+$catalog[101] = [
+    "title" => "The Perks of Being a Wallflower",
+	"img" => "../resources/perks-of-being-a-wallflower-image.jpg",
+    "genre" => "Young Adult",
+    "format" => "Paperback",
+    "year" => 1999,
+    "category" => "Books",
+    "authors" => [
+        "Stephan Chbosky"
+    ],
+    "publisher" => "Pocket Books",
+    "isbn" => '0-671-02734-4'
+];
+
+$catalog[102] = [
+    "title" => "Lord of the Flies",
+	"img" => "../resources/lord-of-the-flies-image.jpg",
+    "genre" => "Allegorical novel",
+    "format" => "Paperback",
+    "year" => 1954,
+    "category" => "Books",
+    "authors" => [
+        "William Golding"
+    ],
+    "publisher" => "Faber and Faber",
+    "isbn" => '0-571-05686-5'
+];
+
+$catalog[103] = [
+    "title" => "Hatchet",
+	"img" => "../resources/hatchet-image.jpeg",
+    "genre" => "Young Adult",
+    "format" => "Paperback",
+    "year" => 1987,
+    "category" => "Books",
+    "authors" => [
+        "Gary Paulsen"
+    ],
+    "publisher" => "Kevin Lain",
+    "isbn" => '0-02-770130-1'
+];
+
+//Movies
+$catalog[201] = [
+    "title" => "Guardians of the Galaxy Vol. 2",
+    "img" => "resources/guardians-of-the-galaxy-2-image.jpg",
+    "genre" => "Superhero",
+    "format" => "Blu-ray",
+    "year" => 2017,
+    "category" => "Movies",
+    "director" => "James Gunn",
+    "writers" => [
+        "James Gunn"
+    ],
+    "stars" => [
+        "Chris Pratt",
+        "Zoe Saldana",
+        "Dave Bautista",
+        "Vin Diesel",
+        "Bradley Cooper",
+        "Michael Rooker",
+        "Karen Gillan",
+        "Pom Klementieff",
+        "Elizabeth Debicki",
+        "Chris Sullivan",
+        "Sean Gunn",
+        "Sylvester Stallone",
+        "Kurt Russell"
+    ]
+];
+
+$catalog[202] = [
+    "title" => "Scott Pilgrim",
+    "img" => "resources/scott-pilgrim-image.jpg",
+    "genre" => "Comedy",
+    "format" => "DVD",
+    "year" => 2010,
+    "category" => "Movies",
+    "director" => "Edgar Wright",
+    "writers" => [
+        "Michael Bacall",
+        "Edgar Wright"
+    ],
+    "stars" => [	
+        "Michael Cera",
+        "Mary Elizabeth Winstead",
+        "Kieran Culkin",
+        "Chris Evans",
+        "Anna Kendrick",
+        "Alison Pill",
+        "Brandon Routh",
+        "Jason Schwartzman"
+    ]
+];
+
+$catalog[203] = [
+    "title" => "Forrest Gump",
+    "img" => "resources/forrest-gump-image.jpg",
+    "genre" => "Drama",
+    "format" => "DVD",
+    "year" => 1994,
+    "category" => "Movies",
+    "director" => "Robert Zemeckis",
+    "writers" => [
+        "Winston Groom",
+        "Eric Roth"
+    ],
+    "stars" => [
+        "Tom Hanks",
+        "Rebecca Williams",
+        "Sally Field",
+        "Michael Conner Humphreys"
+    ]
+];
+
+
+//Music
+$catalog[301] = [
+    "title" => "Airbrushed",
+    "img" => "resources/anamanaguchi-airbrushed-image.jpg",
+    "genre" => "Chiptune",
+    "format" => "CD",
+    "year" => 2010,
+    "category" => "Music",
+    "artist" => "Anamanaguchi"
+];
+
+$catalog[302] = [
+    "title" => "Dawn Metropolis",
+    "img" => "resources/anamanaguchi-dawn-metropolis-image.jpg",
+    "genre" => "Chiptune",
+    "format" => "CD",
+    "year" => 2009,
+    "category" => "Music",
+    "artist" => "Anamanaguchi"
+];
+
+$catalog[303] = [
+    "title" => "Power Supply",
+    "img" => "resources/anamanaguchi-power-supply-image.jpg",
+    "genre" => "Chiptune",
+    "format" => "CD",
+    "year" => 2006,
+    "category" => "Music",
+    "artist" => "Anamanaguchi"
+];
+
+
+?>
